fbb20e04151c6e0b010161a5847a35a835a0ac99
doom <61824552+JustDoom@users.noreply.github.com>
2025-05-03 15:05:59 +1000

    Bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
 allprojects {
     group = "com.imjustdoom.betterkeepinventory"
-    version = "2.0.0"
+    version = "2.0.1"
     description = "BetterKeepInventory"
 
     repositories {
